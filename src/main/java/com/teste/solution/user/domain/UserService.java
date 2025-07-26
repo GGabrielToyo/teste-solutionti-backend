85fdbf90e4742e8aab80f61d487da4fcbd48d283
Gabriel Alves Toyo <ggabrieltoyo@gmail.com>
2025-07-26 14:40:22 -0300

    feat: Mapeamento da entidade user.

---
+++ src/main/java/com/teste/solution/user/domain/UserService.java
@@ -0,0 +1,11 @@
+package com.teste.solution.user.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+}
