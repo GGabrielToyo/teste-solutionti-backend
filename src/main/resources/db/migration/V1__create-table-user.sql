5a9b0c9415497ff873a0ba87dbf0414e0bec7e00
Gabriel Alves Toyo <ggabrieltoyo@gmail.com>
2025-07-28 12:10:40 -0300

    refactor: Alterando o banco de dados para Postgres.

--- src/main/resources/db/migration/V1__create-table-user.sql
+++ src/main/resources/db/migration/V1__create-table-user.sql
@@ -1,6 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE tb_solti_user (
-    solti_user_id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
+    solti_user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     solti_user_name VARCHAR(100) NOT NULL,
     solti_user_cpf VARCHAR(20) NOT NULL UNIQUE,
-    solti_user_created_at DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+    solti_user_created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- src/main/resources/db/migration/V2__create-table-address.sql
+++ src/main/resources/db/migration/V2__create-table-address.sql
@@ -1,5 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE tb_solti_address (
-    solti_address_id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
+    solti_address_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     solti_address_zip_code VARCHAR(20) NOT NULL,
     solti_address_street VARCHAR(255),
     solti_address_complement VARCHAR(255),
@@ -13,9 +15,10 @@ CREATE TABLE tb_solti_address (
     solti_address_gia_code VARCHAR(10),
     solti_address_area_code VARCHAR(3),
     solti_address_siafi_code VARCHAR(10),
-    solti_address_created_at DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
-    solti_address_user_id UNIQUEIDENTIFIER NOT NULL,
+    solti_address_created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    solti_address_user_id UUID NOT NULL,
 
     CONSTRAINT fk_solti_address_user FOREIGN KEY (solti_address_user_id)
     REFERENCES tb_solti_user(solti_user_id)
 );
+
