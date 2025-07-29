0241a6591f9e112b38090f9da3ea45afcf6f2b59
Gabriel Alves Toyo <ggabrieltoyo@gmail.com>
2025-07-28 21:30:42 -0300

    feat: Configurando swagger para api.

---
+++ src/main/java/com/teste/solution/infra/swagger/SwaggerApiConfigurations.java
@@ -0,0 +1,39 @@
+package com.teste.solution.infra.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+public class SwaggerApiConfigurations {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
+                .components(new Components().addSecuritySchemes("bearer-key",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Back-end Intranet Boatlux")
+                        .description("API para teste do processo seletivo da empresa Solution TI.")
+                        .termsOfService("")
+                        .contact(
+                                new Contact()
+                                        .name("Gabriel Toyo [Desenvolvedor]")
+                                        .email("ggabrieltoyo@gmail.com.br")
+                                        .url("https://www.linkedin.com/in/gabriel-alves-toyo-709b72205/"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT"))
+                        .version("1.0.0")
+                        .extensions(Map.of("x-empresa", "Solution TI"))
+                );
+    }
+}
